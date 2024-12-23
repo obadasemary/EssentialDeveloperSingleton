93f8aa56245751ae2e00b7a0fa3f8666326823e6
Abdelrahman Mohamed <obada.semary@gmail.com>
2024-12-23 16:46:05 +0300

    Initial Commit

---
+++ EDSingleton/EDSingletonApp.swift
@@ -0,0 +1,17 @@
+//
+//  EDSingletonApp.swift
+//  EDSingleton
+//
+//  Created by Abdelrahman Mohamed on 23.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct EDSingletonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
