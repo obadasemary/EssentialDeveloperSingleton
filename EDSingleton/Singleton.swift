6ddac511812cd0037b2f5174f6f95db4f307235c
Abdelrahman Mohamed <obada.semary@gmail.com>
2024-12-24 15:11:18 +0300

    chore: implement Example

--- EDSingleton/Singleton.swift
+++ EDSingleton/Singleton.swift
@@ -5,4 +5,58 @@
 //  Created by Abdelrahman Mohamed on 23.12.2024.
 //
 
-import Foundation
+import UIKit
+
+// Main Module
+
+extension ApiClient {
+    func login(completion: (LoggedInUser) -> Void) {}
+}
+
+extension ApiClient {
+    func loadFeed(completion: ([FeedItem]) -> Void) {}
+}
+
+// Api Module
+
+class ApiClient {
+    
+    static let instance = ApiClient()
+    
+    func execute(
+        _ request: URLRequest,
+        completion: @escaping (Result<Data, Error>) -> Void
+    ) {}
+}
+
+class MockApiClint: ApiClient {}
+
+// Login Module
+
+struct LoggedInUser {}
+
+class LoginViewModel {
+    
+    var login: (((LoggedInUser) -> Void) -> Void)?
+    
+    func didTapLoginButton() {
+        login? { user in
+            print(user)
+        }
+    }
+}
+
+// Feed Module
+
+struct FeedItem {}
+
+class FeedViewModel {
+    
+    var loadFeed: ((([FeedItem]) -> Void) -> Void)?
+    
+    func load() {
+        loadFeed? { loadedItems in
+            
+        }
+    }
+}
