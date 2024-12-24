c36505a1083b8c6865654aec757bd7500524cf6a
Abdelrahman Mohamed <obada.semary@gmail.com>
2024-12-24 15:31:33 +0300

    chore: implement `FeedViewController` and `FeedLoader` as a closure

--- EDSingleton/FeedModule/FeedViewController.swift
+++ EDSingleton/FeedModule/FeedViewController.swift
@@ -5,4 +5,26 @@
 //  Created by Abdelrahman Mohamed on 24.12.2024.
 //
 
-import Foundation
+import UIKit
+
+typealias FeedLoader = (([String]) -> Void) -> Void
+
+class FeedViewController: UIViewController {
+    
+    var loadFeed: FeedLoader!
+    
+    convenience init(
+        loadFeed: @escaping FeedLoader
+    ) {
+        self.init()
+        self.loadFeed = loadFeed
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadFeed { [weak self] loadedItems in
+            
+        }
+    }
+}
